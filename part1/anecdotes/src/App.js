e9432f2cfca7ad693a6f6e1f50bb0420611718e0
sentanl869 <sentanl869@gmail.com>
2021-03-23 23:53:30 +0800

    Add part1 d part 1.13 exercise solution.

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react'
 
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.handlerClick}>{props.text}</button>
+    </>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -9,16 +17,25 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
-   
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
+
   const setRandomAnecote = () => {
     const selectNum = Math.floor(Math.random() * anecdotes.length)
     setSelected(selectNum)
   }
+  const setAnecoteVote = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
-      <button onClick={setRandomAnecote}>next anecdotes</button>
+      <p>has {points[selected]} votes</p>
+      <Button handlerClick={setAnecoteVote} text='vote' />
+      <Button handlerClick={setRandomAnecote} text='next anecdotes' />
     </div>
   )
 }
