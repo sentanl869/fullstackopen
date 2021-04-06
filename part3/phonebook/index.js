88d5d88545b28e06425c7a20e68ded6d41c1bff3
sentanl869 <sentanl869@gmail.com>
2021-04-06 17:37:33 +0800

    Add part3 a part 3.7 & 3.8 exercises solution.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+morgan.token('body', (req, res) => {
+  return req.method === 'POST'
+    ? JSON.stringify(req.body)
+    : ' '
+})
+
 app.use(express.json())
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :body'
+))
 
 let persons = [
     {
