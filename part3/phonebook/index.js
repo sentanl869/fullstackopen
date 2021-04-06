10ceeab93936c5cda7231cc202b410eaa8526efc
sentanl869 <sentanl869@gmail.com>
2021-04-06 16:41:57 +0800

    Add part3 a part 3.5 exercise solution.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         id: 1,
@@ -50,6 +52,17 @@ app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
   res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  const person = {
+    id: Math.floor(Math.random()*10000),
+    name: body.name,
+    number: body.number,
+  }
+  persons = persons.concat(person)
+  res.json(person)
 })
 
 const PORT = 3001
