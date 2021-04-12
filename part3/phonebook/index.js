a6a68c6a2ebaf7a7ba5fbbe5a79344dfed95b7ec
sentanl869 <sentanl869@gmail.com>
2021-04-12 21:47:40 +0800

    Add part3 b part 3.9 exercise solution.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 morgan.token('body', (req, res) => {
   return req.method === 'POST'
