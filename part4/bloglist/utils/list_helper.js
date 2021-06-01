d85221a43602c398b35b5ab27a8edcf81a995fd1
sentanl869 <sentanl869@gmail.com>
2021-06-01 23:13:31 +0800

    Add part4 a part 4.4 exercise solution.

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,13 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((result, blog) => result += blog.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes,
 }
