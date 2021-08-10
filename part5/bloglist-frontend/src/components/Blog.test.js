394710225266bd3f543c1461d4983ace08d007a0
sentanl869 <sentanl869@gmail.com>
2021-08-10 23:19:07 +0800

    Add part5 c part 5.14 exercise solution.

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
+describe('<Blog />', () => {
+
   const blog = {
     title: 'test blog title',
     author: 'test blog author',
@@ -15,24 +16,33 @@ test('renders content', () => {
     },
   }
 
-  const updateBlog = jest.fn()
-  const removeBlog = jest.fn()
+  test('renders content', () => {
 
-  const component = render(
-    <Blog key={blog.id} blog={blog} updateBlog={updateBlog} removeBlog={removeBlog} />
-  )
+    const mockHandler = jest.fn()
 
-  expect(component.container).toHaveTextContent(
-    blog.title
-  )
-  expect(component.container).toHaveTextContent(
-    blog.author
-  )
-  expect(component.container).not.toHaveTextContent(
-    blog.url
-  )
-  expect(component.container).not.toHaveTextContent(
-    'likes 666'
-  )
+    const component = render(
+      <Blog key={blog.id} blog={blog} updateBlog={mockHandler} removeBlog={mockHandler} />
+    )
 
+    expect(component.container).toHaveTextContent(blog.title)
+    expect(component.container).toHaveTextContent(blog.author)
+    expect(component.container).not.toHaveTextContent(blog.url)
+    expect(component.container).not.toHaveTextContent('likes 666')
+
+  })
+
+  test('click button to show detail', () => {
+
+    const mockHandler = jest.fn()
+
+    const component = render(
+      <Blog key={blog.id} blog={blog} updateBlog={mockHandler} removeBlog={mockHandler} />
+    )
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent(blog.url)
+    expect(component.container).toHaveTextContent('likes 666')
+  })
 })
