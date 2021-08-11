a92c0beddab4962de2bc601d6deacfbe0d7e6821
sentanl869 <sentanl869@gmail.com>
2021-08-11 21:50:54 +0800

    Add part5 c part 5.15 exercise solution.

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -4,7 +4,6 @@ import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-
   const blog = {
     title: 'test blog title',
     author: 'test blog author',
@@ -15,14 +14,18 @@ describe('<Blog />', () => {
       name: 'test_user_name'
     },
   }
+  const updateBlog = jest.fn()
+  const removeBlog = jest.fn()
+
+  let component
+
+  beforeEach(() => {
+    component = render(
+      <Blog key={blog.id} blog={blog} updateBlog={updateBlog} removeBlog={removeBlog} />
+    )
+  })
 
   test('renders content', () => {
-
-    const mockHandler = jest.fn()
-
-    const component = render(
-      <Blog key={blog.id} blog={blog} updateBlog={mockHandler} removeBlog={mockHandler} />
-    )
 
     expect(component.container).toHaveTextContent(blog.title)
     expect(component.container).toHaveTextContent(blog.author)
@@ -32,17 +35,23 @@ describe('<Blog />', () => {
   })
 
   test('click button to show detail', () => {
-
-    const mockHandler = jest.fn()
-
-    const component = render(
-      <Blog key={blog.id} blog={blog} updateBlog={mockHandler} removeBlog={mockHandler} />
-    )
-
     const button = component.getByText('view')
     fireEvent.click(button)
 
     expect(component.container).toHaveTextContent(blog.url)
     expect(component.container).toHaveTextContent('likes 666')
+  })
+
+  test('like button click twice', () => {
+    const button = component.container.querySelector('button')
+    fireEvent.click(button)
+
+    const likeButton = component.getByText('like')
+
+    fireEvent.click(likeButton)
+    expect(updateBlog.mock.calls).toHaveLength(1)
+
+    fireEvent.click(likeButton)
+    expect(updateBlog.mock.calls).toHaveLength(2)
   })
 })
